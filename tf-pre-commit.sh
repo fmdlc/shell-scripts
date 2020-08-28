611b44b05fef6b854babf7de4c2c8d17e2b908e1
Facu de la Cruz <fmdlc.unix@gmail.com>
2020-08-28 00:55:38 -0300

    Fixing pre-commit hook

--- tf-pre-commit.sh
+++ tf-pre-commit.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Terraform pre-commit hook running"
 # Formats any *.tf files according to the hashicorp convention
 files=$(git diff --cached --name-only)
 
 for f in $files; do
   if [ -e "$f" ] && [[ "${f##*.}" =~ ^(tf|tfvars)$ ]]; then
-    /usr/local/bin/terraform validate
     /usr/local/bin/terraform fmt -check=true "$f"
   fi
 done
